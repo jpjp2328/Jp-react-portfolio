0ed244faeb791370bc9c928e53183cc495507a91
jpjp2328 <110818668+jpjp2328@users.noreply.github.com>
2023-01-19 01:25:43 +1100

    modal not working properly, using alert for now

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,30 +1,34 @@
 import React from 'react';
 
 function ContactForm() {
-    // Get the modal
+
+/*     // Get Modal
     const modal = document.getElementById("myModal");
 
     // Get the button that opens the modal
-    const submitButton = document.getElementById("submit-button");
+    const submitBtn = document.getElementById("submit-button");
 
     // Get the <span> element that closes the modal
     const span = document.getElementsByClassName("close")[0];
 
     // When the user clicks the button, open the modal 
-    submitButton.onClick = function () {
-        modal.style.display = "block";
+    submitBtn.onclick = function () {
+        modal.style.display = "block"
     }
 
     // When the user clicks on <span> (x), close the modal
-    span.onClick = function () {
-        modal.style.display = "none";
+    span.onclick = function () {
+        modal.style.display = "none"
     }
 
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function (event) {
         if (event.target === modal) {
-            modal.style.display = "none";
+            modal.style.display = "none"
         }
+    } */
+    function alertMessage() {
+        alert("Unfortunately this feature is currently unavailable, Please email me instead under More Info.. Thank you")
     }
 
     return (
@@ -41,21 +45,20 @@ function ContactForm() {
                     <label>Message me</label>
                     <textarea id="message" name="subject" placeholder="Leave me a message.."></textarea>
 
-                    <input id="submit-button" type="submit" value="Submit"></input>
+                    <input id="submit-button" onClick={() => alertMessage()} type="submit" value="Submit"></input>
                     <input id="resume-button" type="submit" value="Resume"></input>
                     <p>OR Download my Resume Here</p>
                 </form>
             </div>
-            <div id="myModal" class="modal">
-                <div class="modal-content">
-                    <span class="close">&times;</span>
+            <div id="myModal" className="modal">
+                <div className="modal-content">
+                    <span className="close">&times;</span>
                     <p>Unfortunately this feature is currently unavailable, Please email me instead under More Info.. Thank you</p>
                 </div>
             </div>
         </div>
-
-
     );
+
 };
 
 export default ContactForm;
