1cae36d5aec061e6db8de615fbb52cf46b977509
jpjp2328 <110818668+jpjp2328@users.noreply.github.com>
2023-01-19 03:52:56 +1100

    changed footer href value

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
                 <ul>
                     <li><a href="https://github.com/jpjp2328">GitHub</a></li>
                     <li><a href="https://mail.google.com/mail/u/0/?fs=1&to=jeffreyp2328@gmail.com&su=Hi&body=Leave-me-a-message&tf=cm">Email Me</a></li>
-                    <li><a href="#">LinkedIn</a></li>
+                    <li><a href="https://github.com/jpjp2328">LinkedIn</a></li>
                 </ul>
             </div>
         </footer>
