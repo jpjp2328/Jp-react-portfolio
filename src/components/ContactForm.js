b20af561a2b89fec906fb5e5a892dbf501789234
jpjp2328 <110818668+jpjp2328@users.noreply.github.com>
2023-01-19 01:37:56 +1100

    resume button removed, hyperlink text to download resume instead

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,31 +2,31 @@ import React from 'react';
 
 function ContactForm() {
 
-/*     // Get Modal
-    const modal = document.getElementById("myModal");
-
-    // Get the button that opens the modal
-    const submitBtn = document.getElementById("submit-button");
-
-    // Get the <span> element that closes the modal
-    const span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks the button, open the modal 
-    submitBtn.onclick = function () {
-        modal.style.display = "block"
-    }
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function () {
-        modal.style.display = "none"
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function (event) {
-        if (event.target === modal) {
+    /*     // Get Modal
+        const modal = document.getElementById("myModal");
+    
+        // Get the button that opens the modal
+        const submitBtn = document.getElementById("submit-button");
+    
+        // Get the <span> element that closes the modal
+        const span = document.getElementsByClassName("close")[0];
+    
+        // When the user clicks the button, open the modal 
+        submitBtn.onclick = function () {
+            modal.style.display = "block"
+        }
+    
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function () {
             modal.style.display = "none"
         }
-    } */
+    
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function (event) {
+            if (event.target === modal) {
+                modal.style.display = "none"
+            }
+        } */
     function alertMessage() {
         alert("Unfortunately this feature is currently unavailable, Please email me instead under More Info.. Thank you")
     }
@@ -46,8 +46,9 @@ function ContactForm() {
                     <textarea id="message" name="subject" placeholder="Leave me a message.."></textarea>
 
                     <input id="submit-button" onClick={() => alertMessage()} type="submit" value="Submit"></input>
-                    <input id="resume-button" type="submit" value="Resume"></input>
-                    <p>OR Download my Resume Here</p>
+                    <a href='./assets/files/Jeffrey_Pang_CV.pdf' download="Jeffrey_Pang_CV">
+                        <p>Click "HERE" to Download my Resume</p>
+                    </a>
                 </form>
             </div>
             <div id="myModal" className="modal">
