c37eadbf6a753a0cedc2b94e9379a155e085cdb4
jpjp2328 <110818668+jpjp2328@users.noreply.github.com>
2023-01-18 23:19:06 +1100

    edit styling and format of contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,22 +2,26 @@ import React from 'react';
 
 function ContactForm() {
     return (
-        <div className="form-container">
-            <form>
-                <label>Name</label>
-                <input type="text" id="contact-name" name="name" placeholder="Your name.."></input>
+        <div>
+            <div className="container">
+                <h7> Contact Me...</h7>
+                <form>
+                    <label>Name</label>
+                    <input type="text" id="contact-name" name="name" placeholder="Your name.."></input>
 
-                <label>Email</label>
-                <input type="text" id="contact-email" name="email" placeholder="Your email.."></input>
+                    <label>Email</label>
+                    <input type="text" id="contact-email" name="email" placeholder="Your email.."></input>
 
-                <label>Message me</label>
-                <textarea id="message" name="subject" placeholder="Leave me a message.."></textarea>
+                    <label>Message me</label>
+                    <textarea id="message" name="subject" placeholder="Leave me a message.."></textarea>
 
-                <input id="submit-button" type="submit" value="Submit"></input>
-                <input id="resume-button" type="submit" value="Resume"></input>
-                <p>OR Download my Resume Here</p>
-            </form>
+                    <input id="submit-button" type="submit" value="Submit"></input>
+                    <input id="resume-button" type="submit" value="Resume"></input>
+                    <p>OR Download my Resume Here</p>
+                </form>
+            </div>
         </div>
+
     );
 };
 
